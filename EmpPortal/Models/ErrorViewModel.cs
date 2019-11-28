9caa614abea7e2d38489953ee77bb0f1f3b0f8c5
Prashant Pandey <prashant.pandey@nagarro.com>
2019-11-28 12:36:39 +0530

    initial commit

---
+++ EmpPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmpPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
